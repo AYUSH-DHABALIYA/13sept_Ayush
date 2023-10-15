2d4bbb15d8b46bf9f24a2fc5d310ecb50a1d981b
AYUSH-DHABALIYA <145126146+AYUSH-DHABALIYA@users.noreply.github.com>
2023-10-15 18:03:08 +0530

    dayslogic.c
    day to week...

--- Task/dayslogic.c
+++ Task/dayslogic.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 void main()
 {
-    int a, week,month,year;
+    int a, week,month,year;// create variable // 
     printf("Enter numbers of days :");
-    scanf("%d",&a);
-    week = a/7;
+    scanf("%d",&a);// get value //
+    week = a/7;// get number of week //
     printf("\nThere are %d weeks");
-    month = a/30;
+    month = a/30;// get number of month //
     printf("\nThere are %d months");
-    year = a/365;
+    year = a/365;// get number of year //
     printf("\nThere is %d year");
 
 }
