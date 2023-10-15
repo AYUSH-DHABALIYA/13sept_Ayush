43fa085614435b0337b32b199051ce62ce70c8e0
AYUSH-DHABALIYA <145126146+AYUSH-DHABALIYA@users.noreply.github.com>
2023-10-15 18:04:06 +0530

    evenandodd.c
    even to odd

--- Task/evenandodd.c
+++ Task/evenandodd.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 void main()
 {
-    int a,b;
+    int a,b; // create variable //
     printf("Enter any number :");
-    scanf("%d",&a);
-    if (a%2 == 0)
+    scanf("%d",&a); // get value //
+    if (a%2 == 0) // put condition //
     {
        printf("the number is even");
     }
