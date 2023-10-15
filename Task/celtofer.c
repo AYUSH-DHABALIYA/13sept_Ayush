a8217183ed4b7bfef1bfbea94954a085a302eb04
AYUSH-DHABALIYA <145126146+AYUSH-DHABALIYA@users.noreply.github.com>
2023-10-15 17:59:51 +0530

    celtofer.c
    celc to fah

--- Task/celtofer.c
+++ Task/celtofer.c
@@ -4,9 +4,9 @@ void main()
     float celcius;
     float fahrenheit;
     printf("Enter temprature in celcius : ");
-    scanf("%f",&celcius);
+    scanf("%f",&celcius); // to get value //
     
-    fahrenheit =(float)((celcius*9)/5+32);
+    fahrenheit =(float)((celcius*9)/5+32); // convert celcius into float //
     printf("Temprature in fahreheit is %.1f f",fahrenheit);
 
 }
