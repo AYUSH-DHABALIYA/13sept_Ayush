5214b31319db36a4e16b00e031af29c796973ad1
AYUSH-DHABALIYA <145126146+AYUSH-DHABALIYA@users.noreply.github.com>
2023-10-15 17:51:26 +0530

    agelogic.c

--- Task/agelogic.c
+++ Task/agelogic.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 void main()
 {
-    int prev_age,cur_age,next_age;
+    int prev_age,cur_age,next_age; // create variable //
     printf("enter your current age :");
-    scanf("%d",&cur_age);
+    scanf("%d",&cur_age); // get value //
     prev_age = --cur_age;
     next_age = ++cur_age;
     next_age = ++cur_age;
    
-    printf("\nYour previous year age is: %d",prev_age); 
-    printf("\nYour Next year age is: %d",next_age);  
+    printf("\nYour previous year age is: %d",prev_age); // previous year age //
+    printf("\nYour Next year age is: %d",next_age);  // next year age //
    
 } 
 
