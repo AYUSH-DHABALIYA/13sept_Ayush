bb6dfe5c12fe30d218fad1c7186e4745a2fd18f4
AYUSH-DHABALIYA <145126146+AYUSH-DHABALIYA@users.noreply.github.com>
2023-10-15 18:08:03 +0530

    swapthevalue.c
    swap the value

--- Task/swapthevariable.c
+++ Task/swapthevariable.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 void main()
 {
-    int a, b;
+    int a, b; // create variable //
     printf("\nEnter the first number :",a);
-    scanf("%d",&a);
+    scanf("%d",&a);// get value //
     printf("\nEnter second number :",b);
     scanf("%d",&b);
-    a = a+b;
-    b = a-b;
+    a = a+b; // add a and b //
+    b = a-b; // substract b from a // 
     a = a-b;
     printf("\nNumber you get is %d ",a);
     printf("\nNumber you get is %d ",b);
