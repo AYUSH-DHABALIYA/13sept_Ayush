d44342768a8cd885a558629ce0357bb1144344b5
AYUSH-DHABALIYA <145126146+AYUSH-DHABALIYA@users.noreply.github.com>
2023-10-15 17:57:52 +0530

    calculator.c
    calculation of a and b

--- Task/calculator.c
+++ Task/calculator.c
@@ -3,12 +3,12 @@ void main()
 {
     int a, b;
     printf("Enter the value of a :");
-    scanf("%d",&a); // to get value //
+    scanf("%d",&a); // to get value of a  //
     printf("Enter the value of b :");
-    scanf("%d",&b);
-    printf("\nAddition of number is: %d", a+b); // addition of and b //
-    printf("\nSubstraction of number is: %d", a-b); 
-    printf("\nMultiplication of number is: %d", a*b);
-    printf("\nDivision of number is: %d", a/b);
-    printf("\nModule of number is: %d", a%b);
+    scanf("%d",&b);// to get value of b //
+    printf("\nAddition of number is: %d", a+b); // addition of a and b //
+    printf("\nSubstraction of number is: %d", a-b); // substraction of a and b //
+    printf("\nMultiplication of number is: %d", a*b); // multiplication of a and b //
+    printf("\nDivision of number is: %d", a/b); // division of a and b //
+    printf("\nModule of number is: %d", a%b); // modulo of a and b //
 }
